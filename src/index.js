3bee0ba19812f64850ab66e989ae9e4bbe87c523
Mike543 <p890098@gmail.com>
2022-01-08 15:14:51 +0300

    feat:add file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
-}
+
+    let numToTwenty = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    
+    let numToNinety = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    if (number < 20) {
+        return numToTwenty[number];
+    } else if (number < 100) {
+        let str = numToNinety[Math.floor(number / 10) - 2];
+        return !(number % 10) ? str : str + " " + numToTwenty[number % 10];
+    } else {
+        let str = numToTwenty[Math.floor(number / 100)] + ' ' + 'hundred';
+        return !(number % 100) ? str : str + " " + toReadable(number % 100);
+    }
+};
+
+
